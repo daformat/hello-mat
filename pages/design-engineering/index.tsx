11ee2fb07ac3a59d4b163f492f621659586a85e8
Mathieu Jouhet <mat.jouhet@gmail.com>
2025-09-25 18:29:42 +0200

    feat(design-engineering): add link to Images and Embeds component with enhanced styling

--- pages/design-engineering/index.tsx
+++ pages/design-engineering/index.tsx
@@ -51,6 +51,26 @@ const DesignEngineeringIndex = () => (
       >
         A details (or disclosure) component
       </Link>
+      <Link
+        href={"/design-engineering/component/images-and-embeds"}
+        style={{
+          alignItems: "center",
+          backgroundColor: "var(--color-toolbar-background)",
+          borderRadius: "14px",
+          boxShadow: `0 1px 12px var(--color-shadow-1),
+          inset 0 0 2px 0.75px var(--color-border-2),
+          inset 0 0 0 0.75px var(--color-border-1)`,
+          display: "flex",
+          flexDirection: "row",
+          gap: "8px",
+          justifyContent: "center",
+          padding: "7px",
+          transition: `color 0.2s ease-in-out, background-color 0.2s ease-in-out",
+            boxShadow 0.2s ease-in-out;`,
+        }}
+      >
+        Images and embeds
+      </Link>
     </div>
   </>
 )
