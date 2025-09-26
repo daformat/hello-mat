cc3c36fc4661594fc300a0e6ae543e1df63014be
Mathieu Jouhet <mat.jouhet@gmail.com>
2025-09-26 13:43:16 +0200

    feat(styles, layout): add orientation-based styling and dynamic class handling
    - Introduced orientation-based styles using `:global` for `landscape_primary` orientation.
    - Implemented `useEffect` in `_app.tsx` to handle dynamic body class updates based on screen orientation.
    - Updated `Layout.module.scss` and `TocComponent.module.scss` to support orientation-safe styling.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import "../styles/globals.scss"
 import type { AppProps } from "next/app"
 import { Layout } from "../components/layout/Layout"
+import { useEffect } from "react"
 
 function HelloMat({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const updateOrientation = () => {
+      const body = document.body
+      body.classList.remove(
+        "orientation_landscape_primary",
+        "orientation_landscape_secondary"
+      )
+      if (screen.orientation) {
+        body.classList.add(
+          `orientation_${screen.orientation.type.replace("-", "_")}`
+        )
+      }
+    }
+    updateOrientation()
+    if (screen.orientation) {
+      screen.orientation.addEventListener("change", updateOrientation)
+    }
+    window.addEventListener("orientationchange", updateOrientation)
+    return () => {
+      if (screen.orientation) {
+        screen.orientation.removeEventListener("change", updateOrientation)
+      }
+      window.removeEventListener("orientationchange", updateOrientation)
+    }
+  }, [])
+
   return (
     <Layout>
       <Component {...pageProps} />
